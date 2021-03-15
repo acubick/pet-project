f70732c55adeab3d4e18695f1a7222ffa98da603
acubick <acubick@gmail.com>
2021-03-15 18:26:11 +0200

    Add css-tricks_skewed-background-secondary

--- src/app/css-tricks/tricks/tricks-routing.module.ts
+++ src/app/css-tricks/tricks/tricks-routing.module.ts
@@ -7,6 +7,7 @@ import {ZigZagComponent} from './zig-zag/zig-zag.component';
 import {SkewedHeaderComponent} from './skewed-header/skewed-header.component';
 import {CurvedHeaderComponent} from './curved-header/curved-header.component';
 import {SkewedBackgroundFirstComponent} from './skewed-background-first/skewed-background-first.component';
+import {SkewedBackgroundSecondaryComponent} from './skewed-background-secondary/skewed-background-secondary.component';
 
 
 const routes: Routes = [
@@ -21,8 +22,12 @@ const routes: Routes = [
       },
       {
         path: 'skewed-header', component: SkewedHeaderComponent
-      }, {
+      },
+      {
         path: 'skewed-background-first', component: SkewedBackgroundFirstComponent
+      },
+      {
+        path: 'skewed-background-secondary', component: SkewedBackgroundSecondaryComponent
       },
       {
         path: '', redirectTo: 'zig-zag', pathMatch: 'full'
