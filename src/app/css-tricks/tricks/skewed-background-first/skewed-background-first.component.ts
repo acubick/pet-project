f730387460252c464c6747247dd9719fa51ee12e
acubick <acubick@gmail.com>
2021-03-15 17:55:41 +0200

    Add css-tricks_skewed-background-1

---
+++ src/app/css-tricks/tricks/skewed-background-first/skewed-background-first.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skewed-background-first',
+  templateUrl: './skewed-background-first.component.html',
+  styleUrls: ['./skewed-background-first.component.scss']
+})
+export class SkewedBackgroundFirstComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
