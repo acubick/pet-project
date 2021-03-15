a12a8c8fffa4450ee5b14edac1fc85aaaf25404d
acubick <acubick@gmail.com>
2021-03-15 04:30:17 +0200

    Add css-tricks_tricks_curved-header

---
+++ src/app/css-tricks/tricks/curved-header/curved-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curved-header',
+  templateUrl: './curved-header.component.html',
+  styleUrls: ['./curved-header.component.scss']
+})
+export class CurvedHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/css-tricks/tricks/tricks.module.ts
+++ src/app/css-tricks/tricks/tricks.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { TricksRoutingModule } from './tricks-routing.module';
 import { ZigZagComponent } from './zig-zag/zig-zag.component';
 import { SkewedHeaderComponent } from './skewed-header/skewed-header.component';
+import { CurvedHeaderComponent } from './curved-header/curved-header.component';
 
 
 @NgModule({
   declarations: [
     ZigZagComponent,
-    SkewedHeaderComponent
+    SkewedHeaderComponent,
+    CurvedHeaderComponent
   ],
   imports: [
     CommonModule,
