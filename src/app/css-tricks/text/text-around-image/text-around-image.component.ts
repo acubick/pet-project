0b5b8dc0b663531bbb62d629d4fe32263b24023a
acubick <acubick@gmail.com>
2021-03-23 15:34:45 +0200

    shape-outside dosn't working

---
+++ src/app/css-tricks/text/text-around-image/text-around-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-around-image',
+  templateUrl: './text-around-image.component.html',
+  styleUrls: ['./text-around-image.component.scss']
+})
+export class TextAroundImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
