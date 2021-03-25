b007cac649e16b4a4f20479d062086a74ca4d356
acubick <acubick@gmail.com>
2021-03-25 23:19:44 +0200

    Add css-tricks/tricks/card-hover-effects

--- src/app/css-tricks/hover/hover-routing.module.ts
+++ src/app/css-tricks/hover/hover-routing.module.ts
@@ -1,33 +1,25 @@
-import { NgModule} from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 import {HoverComponent} from './hover.component';
-import {TestFirstComponent} from './test-first/test-first.component';
-import {TestSecondComponent} from './test-second/test-second.component';
-import {TestThreeComponent} from './test-three/test-three.component';
 import {NotFoundComponent} from '../../not-found/not-found.component';
 import {ImageHoverOverlayComponent} from './image-hover-overlay/image-hover-overlay.component';
+import {CardHoverEffectsComponent} from './card-hover-effects/card-hover-effects.component';
 
 
 const routes: Routes = [
   {
     path: '', component: HoverComponent, children: [
-
       {
-        path: 'test-first', component: TestFirstComponent
+        path: 'card-hover-effects', component: CardHoverEffectsComponent
       },
       {
-        path: 'test-second', component: TestSecondComponent
-      },
-      {
-        path: 'test-three', component: TestThreeComponent
-      },{
         path: 'image-hover-overlay', component: ImageHoverOverlayComponent
       },
       {
         path: '', redirectTo: 'image-hover-overlay', pathMatch: 'full'
       },
-      { path: '**', component:  NotFoundComponent}
+      {path: '**', component: NotFoundComponent}
 
     ]
   }
@@ -37,4 +29,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HoverRoutingModule { }
+export class HoverRoutingModule {
+}
--- src/app/css-tricks/hover/hover.module.ts
+++ src/app/css-tricks/hover/hover.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HoverRoutingModule } from './hover-routing.module';
-import { TestFirstComponent } from './test-first/test-first.component';
-import { TestSecondComponent } from './test-second/test-second.component';
-import { TestThreeComponent } from './test-three/test-three.component';
 import { ImageHoverOverlayComponent } from './image-hover-overlay/image-hover-overlay.component';
+import { CardHoverEffectsComponent } from './card-hover-effects/card-hover-effects.component';
 
 
 @NgModule({
   declarations: [
-    TestFirstComponent,
-    TestSecondComponent,
-    TestThreeComponent,
-    ImageHoverOverlayComponent],
+    ImageHoverOverlayComponent,
+    CardHoverEffectsComponent],
   imports: [
     CommonModule,
     HoverRoutingModule
