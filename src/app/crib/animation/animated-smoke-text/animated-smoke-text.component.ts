3b4d6064f82142a67e2bc0b7b10790a5e00f65d7
acubick <acubick@gmail.com>
2021-03-27 05:42:38 +0200

    Edit  animation.ts

--- src/app/crib/animation/animated-smoke-text/animated-smoke-text.component.ts
+++ src/app/crib/animation/animated-smoke-text/animated-smoke-text.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, HostListener, OnInit} from '@angular/core';
+import {Component,  HostListener, OnInit} from '@angular/core';
 import {moveLetter} from '../animation';
 
 
@@ -12,71 +12,23 @@ import {moveLetter} from '../animation';
 })
 export class AnimatedSmokeTextComponent implements OnInit {
   public itemPrefix: Array<string> = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-  public changedBlockIndex: number | null;
   state = [];
 
   @HostListener('mouseover', ['$event']) mouseMove(e: MouseEvent) {
-    // console.log(e.currentTarget['state']);
-    // console.log(e);
-    // this.change();
-
     console.log(this.state);
-    // console.log(this.state.);
-    // console.log(this.changedBlockIndex);
-    // console.log(this.el.nativeElement);
   }
 
-  constructor(private el: ElementRef) {
-
-    // for (const letter of this.text) {
-    //   this.textArr.push(letter);
-    // }
-    //
-    // console.log(this.textArr);
+  constructor() {
   }
 
 
-
-  // TODO: Закончить с анимацией по этому видео
-  // https://www.youtube.com/watch?v=ra5qNKNc95U&list=PLHfvxV8-y-JpjZZoBe70UsMmUAEdW-ouu
 
 
   ngOnInit(): void {
 
-
-    // now, split text into letters
-
-    //  const text = document.querySelector('.text');
-    //  text.innerHTML = text.textContent.replace(/\S/g, '<span>$&</span>');
-    // // now, add active class on hovered <span> tag
-    //  const letters = document.querySelectorAll('span');
-    //  for (let i = 0; i < letters.length; i++) {
-    //    // tslint:disable-next-line:only-arrow-functions
-    //     letters[i].addEventListener('mouseover', function() {
-    //       letters[i].classList.add('active');
-    //     });
-    //   }
   }
 
-  onHover(event: MouseEvent) {
-    // const direction = event.type === 'mouseenter' ? 'in' : 'out';
-    // const host = event.target as HTMLElement;
-    // const w = host.offsetWidth;
-    // const h = host.offsetHeight;
-    //
-    // const x = (event.pageX - host.offsetLeft - (w / 2)) * (w > h ? (h / w) : 1);
-    // const y = (event.pageY - host.offsetTop - (h / 2)) * (h > w ? (w / h) : 1);
-    // const states = ['top', 'right', 'bottom', 'left'];
-    // const side = Math.round((((Math.atan2(y, x) * (180 / Math.PI)) + 180) / 90) + 3) % 4;
-    // this.state = `${direction}-${states[side]}`;
-  }
 
-  onDone(event: AnimationEvent) {
 
-  }
 
-  change() {
-    this.el.nativeElement.backgroundColor = 'yellow';
-    this.el.nativeElement.color = 'red';
-  }
 }
--- src/app/crib/animation/animation.ts
+++ src/app/crib/animation/animation.ts
@@ -9,18 +9,12 @@ import {
 
 
 export let moveLetter = trigger('moveLetter', [
-  // state('false', style({opacity: 1, filter: 'blur(0)', transform: 'translateY(0) translateX(0) rotate(0deg)'})),
-  // state('move', style({
-  //
-  // })),
-  transition('false => move', [
-    animate('2s', keyframes([
-      style({filter: 'blur(0)', transform: 'translateY(0) translateX(0) rotate(0deg)', offset: 0}),
-      // style({ offset: 0.2}),
-      style({ filter: 'blur(5px)',
-        transform: 'translateY(-200px) translateX(300px) rotate(720deg) scale(4)', offset: 0.3}),
-      style({filter: 'blur(10px)',
-        transform: 'translateY(-300px) translateX(300px) rotate(720deg) scale(4)', offset: 1})
+  state('false', style({opacity: 1, transform: 'none' })),
+  transition('move', [
+    animate(1000,  keyframes([
+      style({opacity: 1, filter: 'blur(0)', transform: 'translateY(0) translateX(0) rotate(0deg)', offset: 0}),
+      style({opacity: 0.5, filter: 'blur(5px)', transform: 'translateY(-200px) translateX(300px) rotate(720deg) scale(4)', offset: 0.3}),
+      style({opacity: 1, filter: 'blur(10px)', transform: 'translateY(-300px) translateX(300px) rotate(720deg) scale(4)', offset: 1})
     ]))
 
     // animate(500, style({ opacity: 0, filter: 'blur(10px)', transform: 'transform: translateY(-300px) translateX(300px) rotate(720deg) scale(4)' }))
