b25df13adc345504ed3c1602005dfa13f6aa01d2
acubick <acubick@gmail.com>
2021-03-28 06:06:01 +0300

    Add component css-tricks/hover/flip-hard-hover

--- src/app/css-tricks/hover/hover.module.ts
+++ src/app/css-tricks/hover/hover.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HoverRoutingModule } from './hover-routing.module';
 import { ImageHoverOverlayComponent } from './image-hover-overlay/image-hover-overlay.component';
 import { CardHoverEffectsComponent } from './card-hover-effects/card-hover-effects.component';
+import { FlipHardHoverComponent } from './flip-hard-hover/flip-hard-hover.component';
 
 
 @NgModule({
   declarations: [
     ImageHoverOverlayComponent,
-    CardHoverEffectsComponent],
+    CardHoverEffectsComponent,
+    FlipHardHoverComponent],
   imports: [
     CommonModule,
     HoverRoutingModule
