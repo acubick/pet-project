2865e9e196a1381e911349bccf68f8c09d45de1f
acubick <acubick@gmail.com>
2021-03-28 08:53:06 +0300

    Add component crib/css/masonry-layout-grid

---
+++ src/app/crib/css/masonry-layout-grid/masonry-layout-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-masonry-layout-grid',
+  templateUrl: './masonry-layout-grid.component.html',
+  styleUrls: ['./masonry-layout-grid.component.scss']
+})
+export class MasonryLayoutGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
