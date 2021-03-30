b13a5a7b640faaf7f65873ce7eea56a2179e878b
acubick <acubick@gmail.com>
2021-03-30 23:02:51 +0300

    Add into component social-media-icons method ngOnDestroy()

--- src/app/css-tricks/hover/social-media-icon/social-media-icon.component.ts
+++ src/app/css-tricks/hover/social-media-icon/social-media-icon.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Output} from '@angular/core';
+import {Component, OnDestroy, OnInit, Output} from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 
@@ -8,11 +8,13 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './social-media-icon.component.html',
   styleUrls: ['./social-media-icon.component.scss']
 })
-export class SocialMediaIconComponent implements OnInit {
+export class SocialMediaIconComponent implements OnInit, OnDestroy {
 
   @Output() outTitle = 'Social Media Icon';
 
-
+   head;
+   link;
+   style;
 
   constructor(private title: Title) {
     title.setTitle('CSS 3D Layered Social Media Icon Hover Effects');
@@ -22,13 +24,16 @@ export class SocialMediaIconComponent implements OnInit {
     //! Solution found that page
     //! https://stackoverflow.com/questions/44545163/how-to-add-stylesheet-dynamically-in-angular-2
 
-    const head = document.getElementsByTagName('head')[0];
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
-    link.integrity = 'sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN';
-    link.crossOrigin = 'anonymous';
-    head.appendChild(link);
+    this.head = document.getElementsByTagName('head')[0];
+    this.link = document.createElement('link');
+    this.style = document.createElement('style');
+    this.link.rel = 'stylesheet';
+    this.link.href = 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
+    this.link.integrity = 'sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN';
+    this.link.crossOrigin = 'anonymous';
+    this.head.appendChild(this.link);
   }
-
+   ngOnDestroy() {
+    this.head.removeChild(this.link);
+   }
 }
