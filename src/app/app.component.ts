68699c208d7bd72c086a748d19fdae17864cb236
acubick <acubick@gmail.com>
2021-03-31 19:47:50 +0300

    Add component css-tricks/hover/user-card-interactions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
 }
+
+// ngOnInit(): void {
+//   //! Solution found that page
+//   //! https://stackoverflow.com/questions/44545163/how-to-add-stylesheet-dynamically-in-angular-2
+//
+//   this.head = document.getElementsByTagName('head')[0];
+//   this.link = document.createElement('link');
+//   this.style = document.createElement('style');
+//   this.link.rel = 'stylesheet';
+//   this.link.href = 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
+//   this.link.integrity = 'sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN';
+//   this.link.crossOrigin = 'anonymous';
+//   this.head.appendChild(this.link);
+// }
+// ngOnDestroy() {
+//   this.head.removeChild(this.link);
+// }
