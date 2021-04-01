3f7b3b94467a7fb1f1e3da144c1c96e7ec9b62cc
acubick <acubick@gmail.com>
2021-04-01 07:14:36 +0300

    Add component ctib/animation/text-animation-effects-d (start develop)

--- src/app/crib/animation/animation.module.ts
+++ src/app/crib/animation/animation.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AnimationRoutingModule } from './animation-routing.module';
 import { AnimatedSmokeTextComponent } from './animated-smoke-text/animated-smoke-text.component';
 import {AnimationComponent} from './animation.component';
+import { TextAnimationEffectsDComponent } from './text-animation-effects-d/text-animation-effects-d.component';
 
 
 @NgModule({
   declarations: [
     AnimationComponent,
-    AnimatedSmokeTextComponent
+    AnimatedSmokeTextComponent,
+    TextAnimationEffectsDComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/crib/animation/text-animation-effects-d/text-animation-effects-d.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-animation-effects-d',
+  templateUrl: './text-animation-effects-d.component.html',
+  styleUrls: ['./text-animation-effects-d.component.scss']
+})
+export class TextAnimationEffectsDComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
