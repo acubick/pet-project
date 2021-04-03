456836fab003fbe3607a94349bb32328550b1180
acubick <acubick@gmail.com>
2021-04-03 11:17:24 +0300

    Add  compoent css-tricks/buttons/custom-radio-buttons

--- src/app/css-tricks/buttons/buttons-routing.module.ts
+++ src/app/css-tricks/buttons/buttons-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {NotFoundComponent} from '../../not-found/not-found.component';
 import {ButtonsComponent} from './buttons.component';
 import {RadioButtonComponent} from './radio-button/radio-button.component';
+import {CustomRadioButtonsComponent} from './custom-radio-buttons/custom-radio-buttons.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,9 @@ const routes: Routes = [
 
       {
         path: 'radio-button', component: RadioButtonComponent
+      },
+      {
+        path: 'custom-radio-buttons', component: CustomRadioButtonsComponent
       },
       {
         path: '', redirectTo: 'radio-button', pathMatch: 'full'
---
+++ src/app/css-tricks/buttons/custom-radio-buttons/custom-radio-buttons.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-custom-radio-buttons',
+  templateUrl: './custom-radio-buttons.component.html',
+  styleUrls: ['./custom-radio-buttons.component.scss']
+})
+export class CustomRadioButtonsComponent implements OnInit , OnDestroy {
+
+  head;
+  link;
+  style;
+
+  constructor(private title: Title) {
+    title.setTitle('Custom Radio Buttons With Cool Effect');
+  }
+
+  ngOnInit(): void {
+    //! Solution found that page
+    //! https://stackoverflow.com/questions/44545163/how-to-add-stylesheet-dynamically-in-angular-2
+
+    this.head = document.getElementsByTagName('head')[0];
+    this.link = document.createElement('link');
+    this.style = document.createElement('style');
+    this.link.rel = 'stylesheet';
+    this.link.href = 'https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css';
+    // this.link.integrity = 'sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN';
+    // this.link.crossOrigin = 'anonymous';
+    this.head.appendChild(this.link);
+  }
+  ngOnDestroy() {
+    this.head.removeChild(this.link);
+  }
+
+}
+
+//<link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
