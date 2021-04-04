e74860030c4a55ded333929fd8740e661de8b5fe
acubick <acubick@gmail.com>
2021-04-04 12:46:17 +0300

    Add  compoent crib/animation/progress-bar-animation  (stsrt develop)

---
+++ src/app/crib/animation/progress-bar-animation/progress-bar-animation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar-animation',
+  templateUrl: './progress-bar-animation.component.html',
+  styleUrls: ['./progress-bar-animation.component.scss']
+})
+export class ProgressBarAnimationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
